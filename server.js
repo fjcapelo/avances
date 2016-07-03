43efceb16257a444086df704c9a82c389972b6db
Francisco.Capelo <fjcapelo@gmail.com>
2016-07-03 20:47:59 +0200

    version uno

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ var app             = express();
 // Logging and Parsing
 app.use(express.static(__dirname + '/public'));                 // sets the static files location to public
 app.use('/bower_components',  express.static(__dirname + '/bower_components')); // Use BowerComponents
+app.use(favicon(__dirname + '/public/favicon.ico'));  
 app.use(morgan('dev'));                                         // log with Morgan
 app.use(bodyParser.json());                                     // parse application/json
 app.use(bodyParser.urlencoded({extended: true}));               // parse application/x-www-form-urlencoded
